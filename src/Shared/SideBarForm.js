93101f10016ad2057fc9d1020d0a8bbdd2c41d12
Omarashraf2002 <omarash2020@gmail.com>
2023-11-03 23:40:40 +0200

    Siderbar is added

---
+++ src/Shared/SideBarForm.js
@@ -0,0 +1,29 @@
+import React from "react";
+import Form from "react-bootstrap/Form";
+import "../Style/Form.css";
+const SideBarForm = () => {
+  return (
+    <Form className="form">
+      {["checkbox", "radio"].map((type) => (
+        <div key={`inline-${type}`} className="mb-3">
+          <Form.Check
+            inline
+            label="طلاب الجامعة"
+            name="group1"
+            type={type}
+            id={`inline-${type}-1`}
+          />
+          <Form.Check
+            inline
+            label="من خارج الجامعة"
+            name="group2"
+            type={type}
+            id={`inline-${type}-2`}
+          />
+        </div>
+      ))}
+    </Form>
+  );
+};
+
+export default SideBarForm;
