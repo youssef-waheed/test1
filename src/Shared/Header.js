c77f668a81a31c11a8159efdc6708d1f35bcfa1b
nourhan <norfared13@gmail.com>
2024-02-19 13:54:54 +0200

    nnnoooouuuuurrrrrrrrrrr

--- src/Shared/Header.js
+++ src/Shared/Header.js
@@ -287,9 +287,7 @@ const Header = () => {
             size="sm"
             className="selectmenu"
             onChange={handleCollegeChange} // Attach onChange event handler
-
             value={College}
-
           >
             {colleges.map((college, index) => (
               <option key={index} value={college}>
