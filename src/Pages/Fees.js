f2828a26149952b5afba707836f688714d76df8e
nourhan <norfared13@gmail.com>
2024-02-24 02:27:05 +0200

    fee

--- src/Pages/Fees.js
+++ src/Pages/Fees.js
@@ -1,13 +1,108 @@
-import React, { useState } from "react";
+import React, { useEffect, useState } from "react";
 import Table from "react-bootstrap/Table";
 import Alert from "react-bootstrap/Alert";
 import Form from "react-bootstrap/Form";
+import axios from "axios";
 
-const Fees = () => {
+const Fees = ({ _id }) => {
   const [isDivVisible, setIsDivVisible] = useState(false);
+  const [feesData, setFeesData] = useState([]);
+  const [userData, setuserData] = useState([]);
+  const [feeTypes, setFeeTypes] = useState([]);
+  const[fee,setFee]= useState({
+    id:"",
+    kind:"",
+    paymentType:"",
+    ofMonth:"",
+    ofYear:"",
+    PaymentValueNumber:"",
+    paymentDate:"",
+    paymentValue:"",
+    payment:""
+
+
+  });
+  
 
   const toggleDiv = () => {
     setIsDivVisible(!isDivVisible);
+  };
+
+  useEffect(() => {
+    if (_id) {
+      fetchFeeStatment(_id);
+      fetchFeeTypes();
+      addFee();
+    }
+  }, [_id]);
+  console.log('====================================');
+  console.log(`id for student: ${_id}`);
+  console.log('====================================');
+
+  const fetchFeeStatment = async (_id) => {
+    try {
+      const response = await axios.get(
+        `http://localhost:5000/fees/feeStatement/` + _id
+      );
+      console.log(response);
+      setFeesData(response.data.data.feesData);
+      setuserData(response.data.data.userData);
+      console.log('=================$$$$$$$$$$$===================');
+      console.log(userData);
+      console.log('=======================$$$$$$$$$=============');
+    } catch (error) {
+      console.log(error);
+    }
+  };
+
+  const fetchFeeTypes = async () => {
+    try {
+      const response = await axios.get(`http://localhost:5000/fees/getFeeType`);
+      console.log(response);
+      setFeeTypes(response.data.data.fees);
+    } catch (error) {
+      console.log(error);
+    }
+  };
+
+  const addFee = async () => {
+    
+  
+    try {
+      const response = await axios.post(
+        `http://localhost:5000/fees/addFeesForStudents`,{
+          id:_id,
+          kind:fee.kind,
+          paymentType:fee.paymentType,
+          ofMonth:fee.ofMonth,
+          ofYear:fee.ofYear,
+          PaymentValueNumber:fee.PaymentValueNumber,
+          paymentDate:fee.paymentDate,
+          paymentValue:fee.paymentValue,
+          payment:fee.payment
+
+
+        }
+      ).then((response) => {
+        setFee({
+          ...fee,
+          id:"",
+          kind:"",
+          paymentType:"",
+          ofMonth:"",
+          ofYear:"",
+          PaymentValueNumber:"",
+          paymentDate:"",
+          paymentValue:"",
+          payment:""
+
+        })
+      })
+      console.log(response);
+    } 
+    catch (error) {
+      console.log(error);
+    }
   };
 
   return (
@@ -22,14 +117,22 @@ const Fees = () => {
         </button>
         {isDivVisible && (
           <div style={{ fontWeight: "bold" }}>
-            <p>الإسم: عمر أشرف إسماعيل</p>{" "}
+            <p>الإسم: {userData.studentName}</p>{" "}
+            <div className="select1">
+              <p>النوع </p>
+              <Form.Select size="sm" className="Type" m-5>
+                {feeTypes.map((type, index) => (
+                  <option key={index}>{type.feeType}</option>
+                ))}
+              </Form.Select>
+            </div>
             <div className="select1">
               <p>نوع الدفع</p>
               <Form.Select size="sm" className="Type" m-5>
-                {" "}
-                الجزاءات
-                <option>شهري </option>
-                <option>سنوي </option>
+                {/* {" "} */}
+                <option>شهري</option>
+                <option>سنوي</option>
+                
               </Form.Select>
             </div>
             <div className="select1">
@@ -71,12 +174,11 @@ const Fees = () => {
                 <option> يسدد الطالب </option>
               </Form.Select>
             </div>
-            <button style={{ backgroundColor: "green", color: "white" }}>
+            <button style={{ backgroundColor: "green", color: "white" }} onClick={addFee}>
               حفظ
             </button>
           </div>
         )}
-            
       </div>{" "}
       <Table striped bordered hover size="sm">
         <thead>
@@ -87,11 +189,14 @@ const Fees = () => {
           </tr>
         </thead>
         <tbody>
-          <tr>
-            <td></td>
-            <td></td>
-            <td></td>
-          </tr>
+          {feesData.map((fee, index) => (
+            <tr key={index}>
+              <td> {fee.kind} </td>
+              <td> {fee.paymentDate} </td>
+              <td> {fee.paymentValue} </td>
+            </tr>
+          ))}
+          <tr></tr>
         </tbody>
       </Table>
       <div className="warning">
