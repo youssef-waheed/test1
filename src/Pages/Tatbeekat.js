b391975b0b66816ee06f91a2c6304eb76c6f5e6f
Omarashraf2002 <omarash2020@gmail.com>
2024-02-27 23:49:35 +0200

    ,,

--- src/Pages/Tatbeekat.js
+++ src/Pages/Tatbeekat.js
@@ -1,6 +1,7 @@
 import React, { useState } from "react";
 import "./Tatbeekat.css";
 import Review from "./ReviewOnlineRequests";
+import AcceptSpecialCases from "./AcceptSpecialCases";
 
 const Page1 = () => (
   <div>
@@ -21,6 +22,11 @@ const Page3 = () => (
   </div>
 );
 
+const Page5 = () => {
+  <div>
+    <AcceptSpecialCases />
+  </div>;
+};
 const ButtonDisplay = ({ buttons, handleClick }) => (
   <div>
     <h2>تطبيقات</h2>
@@ -40,7 +46,7 @@ const App = () => {
     { text: "تنسيق", page: <Page2 /> },
     { text: "سحب الصور", page: <Page3 /> },
     { text: "تصريح جماعي", page: <Page3 /> },
-    { text: "قبول حالات خاصة", page: <Page3 /> },
+    { text: "قبول حالات خاصة", page: <Page5 /> },
     { text: "حجز وجبات (اكسل)", page: <Page3 /> },
     { text: "رفع الصور", page: <Page3 /> },
     { text: "جزاء جماعي", page: <Page3 /> },
