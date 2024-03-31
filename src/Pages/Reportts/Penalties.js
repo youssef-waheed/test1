2a495bedf7d3908a2a8da67ba835bc87679c5620
Omarashraf2002 <omarash2020@gmail.com>
2024-03-31 17:36:38 +0200

    Apsence&Permission and Penalties in Reports are done

--- src/Pages/Reportts/Penalties.js
+++ src/Pages/Reportts/Penalties.js
@@ -2,79 +2,57 @@ import axios from "axios";
 import React, { useEffect, useState } from "react";
 import Form from "react-bootstrap/Form";
 import Table from "react-bootstrap/Table";
+
 const Penalties = () => {
   const [ofYear, setOfYear] = useState("");
-  const [students, setStudents] = useState("");
-  const [selectedPenalty, setSelectedPenalty] = useState([]);
+  const [selectedPenalty, setSelectedPenalty] = useState("");
   const [penalties, setPenalties] = useState([]);
-  const [selectedFilter, setSelectedFilter] = useState("pendingApplications");
+  const [PenaltyDate, setPenaltyDate] = useState(""); // State for PenaltyDate
+  const [cancellationDate, setCancellationDate] = useState(""); // State for cancellationDate
+
   const penaltyKinds = ["جزاء اداري", "جزاء سلوكي"];
-  //   const [checkboxes, setCheckboxes] = useState(() => {
-  //     const storedCheckboxes = JSON.parse(sessionStorage.getItem("checkboxes"));
-  //     return (
-  //       storedCheckboxes || [
-  //         { label: "مصرى", checked: false },
-  //         { label: "وافد", checked: false },
-  //         { label: "متقدمين", checked: false },
-  //         { label: "مقبولين", checked: false },
-  //         { label: "مرفوضين", checked: false },
-  //         { label: "قدامى", checked: false },
-  //         { label: "جدد", checked: false },
-  //         { label: "سكن عادى", checked: false },
-  //         { label: "سكن مميز", checked: false },
-  //         { label: "مسلم", checked: false },
-  //         { label: "مسيحى", checked: false },
-  //         { label: "ساكنى العام السابق", checked: false },
-  //         { label: "فى انتظار التنسيق", checked: false },
-  //       ]
-  //     );
-  //   });
-  var PenaltyDate;
-  var cancellationDate;
-  var penaltyKind;
+
   useEffect(() => {
     fetchPenaltiesReport();
-  }, [ofYear, selectedPenalty]); // Pass 'selectedPenalty' as a dependency
+  }, [ofYear, selectedPenalty, PenaltyDate, cancellationDate]); // Update useEffect dependencies
 
   const fetchPenaltiesReport = async () => {
     const queryString = `?ofYear=${ofYear}&penaltyKind=${selectedPenalty}&PenaltyDate=${PenaltyDate}&cancellationDate=${cancellationDate}`;
-    if (PenaltyDate || cancellationDate || ofYear || penaltyKind) {
-      try {
-        const response = await axios.get(
-          `http://localhost:5000/reports/penalty${queryString}`
-        );
-        setPenalties(response.data.data.users); // Update to set penalties state
-        console.log(response);
-      } catch (error) {
-        console.log(error);
-      }
-    } else {
-      try {
-        const response = await axios.get(
-          `http://localhost:5000/reports/penalty`
-        );
-        setPenalties(response.data.data.users); // Update to set penalties state
 
-        console.log(response);
-      } catch (error) {
-        console.log(error);
-      }
+    try {
+      const response = await axios.get(
+        `http://localhost:5000/reports/penalty${queryString}`
+      );
+      setPenalties(response.data.data.users); // Update to set penalties state
+    } catch (error) {
+      console.log(error);
     }
   };
+
   function handleYearChange(event) {
     const selectedYear = event.target.value;
     setOfYear(selectedYear);
-    // Call fetchPenaltiesReport directly after setting the year state
-    fetchPenaltiesReport();
   }
+
   function handlePenaltyKind(event) {
     const selectedPenaltyKind = event.target.value;
     setSelectedPenalty(selectedPenaltyKind);
-    console.log(selectedPenaltyKind);
   }
+
+  // Function to handle Penalty Date change
+  function handlePenaltyDateChange(event) {
+    const selectedDate = event.target.value;
+    setPenaltyDate(selectedDate);
+  }
+
+  // Function to handle cancellation Date change
+  function handleCancellationDateChange(event) {
+    const selectedDate = event.target.value;
+    setCancellationDate(selectedDate);
+  }
+
   return (
     <div>
-      {" "}
       <div className="two-column-wrapper">
         <div className="col">
           <div className="select">
@@ -109,20 +87,24 @@ const Penalties = () => {
               ))}
             </Form.Select>
           </div>
-          {/* {checkboxes.map((checkbox, index) => (
-        <div key={index} className="checkbox-row">
-          <label className="radio-label">
+          {/* Penalty Date */}
+          <div className="penalty-date">
+            <p>تاريخ الجزاء</p>
             <input
-              type="radio"
-              name="filter"
-              checked={checkbox.checked}
-              onChange={() => handleRadioButtonChange(index)}
-              className="radio-button"
+              type="date"
+              onChange={handlePenaltyDateChange}
+              value={PenaltyDate}
             />
-            {checkbox.label}
-          </label>
-        </div>
-      ))} */}
+          </div>
+          {/* Cancellation Date */}
+          <div className="cancellation-date">
+            <p>تاريخ الإلغاء</p>
+            <input
+              type="date"
+              onChange={handleCancellationDateChange}
+              value={cancellationDate}
+            />
+          </div>
         </div>
         <div className="coll">
           <Table striped bordered hover size="sm">
