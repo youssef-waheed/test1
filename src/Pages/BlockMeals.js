b7e6b9b98d99f2594d487078ca3ec2fa1f0b6a59
Omarashraf2002 <omarash2020@gmail.com>
2024-04-24 21:24:09 +0200

    changes

---
+++ src/Pages/BlockMeals.js
@@ -0,0 +1,176 @@
+import axios from "axios";
+import React, { useEffect, useState } from "react";
+import Table from "react-bootstrap/Table";
+import Alert from "react-bootstrap/Alert";
+import Form from "react-bootstrap/Form";
+import { getAuthUser } from "../helper/storage";
+const auth = getAuthUser();
+const BlockMeals = ({ _id }) => {
+  const [students, setStudents] = useState([]);
+  const [isDivVisible, setIsDivVisible] = useState(false);
+  const [blockMeal, setBlockMeal] = useState({
+    dateTo: "",
+    dateFrom: "",
+    meals: "",
+  });
+
+  useEffect(() => {
+    if (_id) {
+      fetchBlockMeals();
+    }
+  }, [_id]);
+  const toggleDiv = () => {
+    setIsDivVisible(!isDivVisible);
+  };
+  const fetchBlockMeals = async () => {
+    try {
+      const response = await axios.get(
+        `http://localhost:5000/blockMeals/` + _id
+      );
+      console.log(response);
+    } catch (error) {
+      console.log(error);
+    }
+  };
+
+  const addBlockMeal = async () => {
+    try {
+      console.log("Sending data: ", {
+        dateTo: blockMeal.dateTo,
+        dateFrom: blockMeal.dateFrom,
+        meals: blockMeal.meals,
+      });
+      const response = await axios.post(
+        `http://localhost:5000/blockMeals/` + _id,
+        {
+          dateTo: blockMeal.dateTo,
+          dateFrom: blockMeal.dateFrom,
+          meals: blockMeal.meals,
+        }
+      );
+      setBlockMeal({
+        dateTo: "",
+        dateFrom: "",
+        meals: "",
+      });
+      createLogs();
+      incremented();
+    } catch (error) {
+      console.log(error);
+      console.log(_id);
+    }
+  };
+  const incremented = async () => {
+    try {
+      const inc = await axios.put(
+        `http://localhost:5000/logs/increment/${auth.log.adminID}`,
+        {
+          type: "add",
+        }
+      );
+    } catch (error) {
+      console.log(error);
+    }
+  };
+  const createLogs = async () => {
+    try {
+      const logs = await axios.post("http://localhost:5000/logs/createLogs", {
+        adminID: auth.log.adminID,
+        adminUserName: auth.log.adminUserName,
+        action: "اضافة حجز وجبات",
+        objectName: `للطالب ${students.studentName},برقم الطالب ${students.nationalID}`,
+      });
+    } catch (error) {
+      console.log(error);
+    }
+  };
+
+  return (
+    <div>
+      <div>
+        <button
+          onClick={toggleDiv}
+          className="button"
+          style={{ backgroundColor: "blue", color: "white" }}
+        >
+          إضافة
+        </button>
+        {isDivVisible && (
+          <div style={{ fontWeight: "bold" }}>
+            <div className="select1">
+              <Form.Label>الوجبات </Form.Label>
+              <Form.Control
+                type="text"
+                className="Type"
+                onChange={(e) => {
+                  setBlockMeal({ ...blockMeal, meals: e.target.value });
+                }}
+              />
+            </div>
+            <div className="select1">
+              <Form.Label>من تاريخ </Form.Label>
+              <Form.Control
+                type="text"
+                className="Type"
+                onChange={(e) => {
+                  setBlockMeal({ ...blockMeal, dateFrom: e.target.value });
+                }}
+              />
+            </div>
+            <div className="select1">
+              <Form.Label>تاريخ الاخلاء </Form.Label>
+              <Form.Control
+                type="text"
+                className="Type"
+                onChange={(e) => {
+                  setBlockMeal({ ...blockMeal, dateTo: e.target.value });
+                }}
+              />
+            </div>
+            <button
+              style={{ backgroundColor: "green", color: "white" }}
+              onClick={addBlockMeal}
+            >
+              حفظ
+            </button>
+          </div>
+        )}
+            
+      </div>{" "}
+      <Table striped bordered hover size="sm">
+        <thead>
+          <tr>
+            <th>نوع الوجبة</th>
+            <th>من تاريخ</th>
+            <th>إلى تاريخ </th>
+          </tr>
+        </thead>
+        <tbody>
+          {students.map((meal, index) => (
+            <tr key={index}>
+              <td>{meal.penaltyKind}</td>
+              <td>{new Date(meal.dateFrom).toLocaleDateString()}</td>
+              <td>{new Date(meal.dateTo).toLocaleDateString()}</td>
+            </tr>
+          ))}
+        </tbody>
+      </Table>
+      {students.length === 0 && (
+        <div className="warning">
+          <Alert
+            variant="danger"
+            style={{
+              textAlign: "center",
+              fontSize: "22px",
+              fontWeight: "bold",
+            }}
+          >
+            لا يوجد بيانات لهذا الطالب/طالبة{" "}
+          </Alert>
+        </div>
+      )}
+    </div>
+  );
+};
+
+export default BlockMeals;
--- src/Pages/Explusion.js
+++ src/Pages/Explusion.js
@@ -1,50 +1,134 @@
 import axios from "axios";
 import React, { useEffect, useState } from "react";
+import Table from "react-bootstrap/Table";
+import Alert from "react-bootstrap/Alert";
+import Form from "react-bootstrap/Form";
 
-const Explusion = () => {
+const Explusion = ({ _id }) => {
+  const penaltyKinds = ["جزاء اداري", "جزاء سلوكي"];
+  const [selectedPenalty, setSelectedPenalty] = useState([]);
   const [students, setStudents] = useState({
-    ofYear: "",
+    roomId: "",
+    penaltyKind: "",
+    reason: "",
   });
 
-  useEffect(() => {
-    fetchExplusionStudents();
-  }, [students.ofYear]); // Update the dependency array to include students.ofYear
-
-  const fetchExplusionStudents = async () => {
+  const addExplusion = async () => {
     try {
-      console.log("Sending Data:", {
-        ofYear: students.ofYear,
+      console.log("Sending data:", {
+        roomId: students.roomId,
+        penaltyKind: students.penaltyKind,
+        reason: students.reason,
       });
-      const response = await axios.get("http://localhost:5000/expulsion/", {
-        params: {
-          ofYear: students.ofYear,
-        },
-      });
-      console.log(response.data); // Assuming the response data contains the students
+      const response = await axios.post(
+        `http://localhost:5000/expulsion/male/` + _id,
+        {
+          roomId: students.roomId,
+          penaltyKind: students.penaltyKind,
+          reason: students.reason,
+        }
+      );
+      console.log(response);
       setStudents({
-        ofYear: "", // Clearing the year after successful fetch (if needed)
+        roomId: "",
+        penaltyKind: "",
+        reason: "",
       });
+      setSelectedPenalty([]);
     } catch (error) {
       console.log(error);
     }
   };
 
-  // Update the onChange handler to set the selected year
-  const handleYearChange = (e) => {
-    setStudents({
-      ofYear: e.target.value,
-    });
-  };
+  function handlePenaltyKind(event) {
+    const selectedPenaltyKind = event.target.value;
+    setSelectedPenalty(selectedPenaltyKind);
+    console.log(selectedPenaltyKind);
+  }
 
   return (
     <div>
-      {/* Add the select element for year selection */}
-      <select value={students.ofYear} onChange={handleYearChange}>
-        <option value="">Select Year</option>
-        <option value="2022">2022</option>
-        <option value="2023">2023</option>
-        {/* Add more options if needed */}
-      </select>
+      <div>
+        <div style={{ fontWeight: "bold" }}>
+          <div className="select1">
+            <p>نوع الجزاء</p>
+            <Form.Select
+              size="sm"
+              className="Type"
+              m-5
+              onChange={handlePenaltyKind}
+              value={selectedPenalty} // Update value attribute to selectedPenalty
+            >
+              <option>اختر نوع الجزاء...</option>
+              {penaltyKinds.map((penalty, index) => (
+                <option key={index} value={penalty}>
+                  {penalty}
+                </option>
+              ))}
+            </Form.Select>
+          </div>
+          <div className="select1">
+            <Form.Label>السبب </Form.Label>
+            <Form.Control
+              type="text"
+              className="Type"
+              onChange={(e) => {
+                setStudents({ ...students, reason: e.target.value });
+              }}
+            />
+          </div>
+          <div className="select1">
+            <Form.Label>الغرفة </Form.Label>
+            <Form.Control
+              type="text"
+              className="Type"
+              onChange={(e) => {
+                setStudents({ ...students, roomId: e.target.value });
+              }}
+            />
+          </div>
+
+          <button
+            style={{ backgroundColor: "green", color: "white" }}
+            onClick={addExplusion}
+          >
+            حفظ
+          </button>
+        </div>
+            
+      </div>{" "}
+      {/* <Table striped bordered hover size="sm">
+        <thead>
+          <tr>
+            <th>النوع</th>
+            <th>السبب</th>
+            <th>التاريخ </th>
+          </tr>
+        </thead>
+        <tbody>
+          {penalties.map((pen, index) => (
+            <tr key={index}>
+              <td>{pen.penaltyKind}</td>
+              <td>{pen.reason}</td>
+              <td>{new Date(pen.PenaltyDate).toLocaleDateString()}</td>
+            </tr>
+          ))}
+        </tbody>
+      </Table>
+      {penalties.length === 0 && (
+        <div className="warning">
+          <Alert
+            variant="danger"
+            style={{
+              textAlign: "center",
+              fontSize: "22px",
+              fontWeight: "bold",
+            }}
+          >
+            لا يوجد بيانات لهذا الطالب/طالبة{" "}
+          </Alert>
+        </div>
+      )} */}
     </div>
   );
 };
