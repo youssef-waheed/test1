efe10bff2020c8ff1262f3309697ba2c785b45b6
Omarashraf2002 <omarash2020@gmail.com>
2024-06-03 16:25:19 +0300

    AAAAAAAAAAAAAA33333333333333333333

--- src/Shared/UserNav.js
+++ src/Shared/UserNav.js
@@ -8,31 +8,27 @@ import Logo from "./HL.jpeg";
 
 const UserNav = () => {
   return (
- <Nav className="Nav">
-             <Navbar.Brand href="/" className="custom-logo" >
-            <img src={Logo} alt="Logo" className="custom-logo-img" />
-          </Navbar.Brand>
-            <Nav.Link as={Link} to="/AppDate" className="custom-link">
-              مواعيد التقدم للمدن الجامعية
-            </Nav.Link>
-            <Nav.Link as={Link} to="/" className="custom-link">
-              تسجيل الدخول للطلاب
-            </Nav.Link>
-            <Nav.Link
-              as={Link}
-              to="/InquiryAboutAdmission"
-              className="custom-link"
-            >
-              الاستعلام عن القبول في المدينة الجامعية
-            </Nav.Link>
-            <Nav.Link as={Link} to="/AppForm" className="custom-link">
-              تقديم طلب التحاق للمدن الجامعية
-            </Nav.Link>
-            <Nav.Link as={Link} to="/Instructions" className="custom-link">
-            إرشادات التقدم والإقرارت
-            </Nav.Link>
-          </Nav>
-);
+    <Nav className="Nav">
+      <Navbar.Brand href="/" className="custom-logo">
+        <img src={Logo} alt="Logo" className="custom-logo-img" />
+      </Navbar.Brand>
+      <Nav.Link as={Link} to="/AppDate" className="custom-link">
+        مواعيد التقدم للمدن الجامعية
+      </Nav.Link>
+      <Nav.Link as={Link} to="/" className="custom-link">
+        تسجيل الدخول العاملين
+      </Nav.Link>
+      <Nav.Link as={Link} to="/InquiryAboutAdmission" className="custom-link">
+        الاستعلام عن القبول في المدينة الجامعية
+      </Nav.Link>
+      <Nav.Link as={Link} to="/AppForm" className="custom-link">
+        تقديم طلب التحاق للمدن الجامعية
+      </Nav.Link>
+      <Nav.Link as={Link} to="/Instructions" className="custom-link">
+        إرشادات التقدم والإقرارت
+      </Nav.Link>
+    </Nav>
+  );
 };
 
 export default UserNav;
